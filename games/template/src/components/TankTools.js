106490565d536c3c4ea4da219caf49c627f60ec5
John Goalby <jgoalby@gmail.com>
2024-01-05 20:54:22 -0800

    Update TankTools.js

--- games/template/src/components/TankTools.js
+++ games/template/src/components/TankTools.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import Phaser from './lib/phaser.js';
 
 export default class TankTools extends Phaser.Physics.Arcade.Sprite {
   constructor(mainScene, x, y, texture) {
