f81db37f37ddc5cac19e69e29c9fd7cb59f1b40d
John Goalby <jgoalby@gmail.com>
2024-01-06 06:17:58 +0000

    Various changes

--- games/template/src/main.js
+++ games/template/src/main.js
@@ -5,17 +5,28 @@ console.log('hello from main.js');
 console.log('hello from main.js');
 import config from './config/config.js';
 console.log('hello from main.js');
-/*import BootScene from './scenes/BootScene.js';
+import BootScene from './scenes/BootScene.js';
+console.log('hello from main.js');
 import PreloaderScene from './scenes/PreloaderScene.js';
+console.log('hello from main.js');
 import InputScene from './scenes/GetInputScene.js';
+console.log('hello from main.js');
 import MenuScene from './scenes/MenuScene.js';
+console.log('hello from main.js');
 import OptionsScene from './scenes/OptionsScene.js';
+console.log('hello from main.js');
 import CreditsScene from './scenes/CreditsScene.js';
+console.log('hello from main.js');
 import LeaderboardScene from './scenes/LeaderboardScene.js';
+console.log('hello from main.js');
 import IntroScene from './scenes/IntroScene.js';
+console.log('hello from main.js');
 import GameScene from './scenes/GameScene.js';
+console.log('hello from main.js');
 import GameOverScene from './scenes/GameOverScene.js';
-import AudioModel from './utils/audio-status.js';
+console.log('hello from main.js');
+/*import AudioModel from './utils/audio-status.js';
+console.log('hello from main.js');
 
 export default class Game extends Phaser.Game {
   constructor() {
