0f88a4f73214a2fa0883652c9d826ebff4b52dc4
John Goalby <jgoalby@gmail.com>
2024-01-07 07:25:46 +0000

    Clutching

--- games/template/src/scenes/PreloaderScene.js
+++ games/template/src/scenes/PreloaderScene.js
@@ -1,9 +1,38 @@
 //import Constants from '../utils/constants';
 
-console.log("Hello world");
-//console.log(Constants.GAME_WIDTH);
-//console.log(Constants.GAME_HEIGHT);
-//console.log(Constants.ASSETS_PATH);
+class Constants {
+  static get GAME_WIDTH() {
+    return 800;
+  }
+
+  static get GAME_HEIGHT() {
+    return 600;
+  }
+
+  static get GAME_BACKGROUND_COLOR() {
+    return '#000';
+  }
+
+  static get GAME_TYPE() {
+    return Phaser.AUTO;
+  }
+
+  static get GAME_PARENT() {
+    return 'game';
+  }
+
+  static get GAME_TITLE() {
+    return 'Game';
+  }
+
+  static get ASSETS_PATH() {
+    return './src/Assets/';
+  }
+}
+
+console.log(Constants.GAME_WIDTH);
+console.log(Constants.GAME_HEIGHT);
+console.log(Constants.ASSETS_PATH);
 
 export default class PreloaderScene extends Phaser.Scene {
   constructor() {
--- games/template/src/utils/constants.js
+++ games/template/src/utils/constants.js
@@ -1,29 +1,29 @@
 export default class Constants {
-    static get GAME_WIDTH() {
-        return 800;
-    }
-    
-    static get GAME_HEIGHT() {
-        return 600;
-    }
-    
-    static get GAME_BACKGROUND_COLOR() {
-        return '#000';
-    }
-    
-    static get GAME_TYPE() {
-        return Phaser.AUTO;
-    }
-    
-    static get GAME_PARENT() {
-        return 'game';
-    }
-    
-    static get GAME_TITLE() {
-        return 'Game';
-    }
-    
-    static get ASSETS_PATH() {
-        return './src/Assets/';
-    }
+  static get GAME_WIDTH() {
+    return 800;
+  }
+
+  static get GAME_HEIGHT() {
+    return 600;
+  }
+
+  static get GAME_BACKGROUND_COLOR() {
+    return '#000';
+  }
+
+  static get GAME_TYPE() {
+    return Phaser.AUTO;
+  }
+
+  static get GAME_PARENT() {
+    return 'game';
+  }
+
+  static get GAME_TITLE() {
+    return 'Game';
+  }
+
+  static get ASSETS_PATH() {
+    return './src/Assets/';
+  }
 }
