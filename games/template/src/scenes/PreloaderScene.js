14e7fef4bbb5a894d10f83dc2eedda7006480f1b
John Goalby <jgoalby@gmail.com>
2024-01-07 07:28:21 +0000

    Sheesh!

--- games/template/src/scenes/PreloaderScene.js
+++ games/template/src/scenes/PreloaderScene.js
@@ -1,34 +1,4 @@
-//import Constants from '../utils/constants';
-
-class Constants {
-  static get GAME_WIDTH() {
-    return 800;
-  }
-
-  static get GAME_HEIGHT() {
-    return 600;
-  }
-
-  static get GAME_BACKGROUND_COLOR() {
-    return '#000';
-  }
-
-  static get GAME_TYPE() {
-    return Phaser.AUTO;
-  }
-
-  static get GAME_PARENT() {
-    return 'game';
-  }
-
-  static get GAME_TITLE() {
-    return 'Game';
-  }
-
-  static get ASSETS_PATH() {
-    return './src/Assets/';
-  }
-}
+import Constants from '../utils/constants.js';
 
 console.log(Constants.GAME_WIDTH);
 console.log(Constants.GAME_HEIGHT);
