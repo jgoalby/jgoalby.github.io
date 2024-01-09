0c1257e5cd5849c0b218b8f5b2a6a2456e817817
John Goalby <jgoalby@gmail.com>
2024-01-09 03:02:15 +0000

    Weird?

--- games/template/src/config/config.js
+++ games/template/src/config/config.js
@@ -2,10 +2,10 @@ import Constants from '../constants.js';
 
 const MAX_SIZE_WIDTH_SCREEN = 1920
 const MAX_SIZE_HEIGHT_SCREEN = 1080
-const MIN_SIZE_WIDTH_SCREEN = 270
-const MIN_SIZE_HEIGHT_SCREEN = 480
-const SIZE_WIDTH_SCREEN = 540
-const SIZE_HEIGHT_SCREEN = 960
+const MIN_SIZE_WIDTH_SCREEN = 480
+const MIN_SIZE_HEIGHT_SCREEN = 270
+const SIZE_WIDTH_SCREEN = 960
+const SIZE_HEIGHT_SCREEN = 540
 
 export default {
   type: Constants.GAME_TYPE,
