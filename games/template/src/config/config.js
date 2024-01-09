b1c54f57250b92b177ac1158ae3ff00b04b7085f
John Goalby <jgoalby@gmail.com>
2024-01-09 04:06:15 +0000

    Maybe?

--- games/template/src/config/config.js
+++ games/template/src/config/config.js
@@ -9,8 +9,8 @@ const SIZE_HEIGHT_SCREEN = 1080
 
 export default {
   type: Constants.GAME_TYPE,
-  width: 1280,
-  height: 1024,
+  width: window.innerWidth,
+  height: window.innerHeight,
   backgroundColor: 'black',
   scale: {
     mode: Phaser.Scale.ENVELOP,
