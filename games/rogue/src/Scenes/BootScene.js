64da4178af520584bc9675cad603b4286bf0b38e
John Goalby <jgoalby@gmail.com>
2024-01-15 19:15:36 -0800

    Update BootScene.js

--- games/rogue/src/Scenes/BootScene.js
+++ games/rogue/src/Scenes/BootScene.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-unresolved */
 
 //import Phaser from 'phaser';
-import madeBy from '../Assets/misc/me-min.png';
-import gameLogo from '../Assets/misc/taklogo.png';
+//import madeBy from '../Assets/misc/me-min.png';
+//import gameLogo from '../Assets/misc/taklogo.png';
 
 export default class BootScene extends Phaser.Scene {
   constructor() {
@@ -10,8 +10,8 @@ export default class BootScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('madeBy', madeBy);
-    this.load.image('gameLogo', gameLogo);
+    this.load.image('madeBy', '../Assets/misc/me-min.png');
+    this.load.image('gameLogo', '../Assets/misc/taklogo.png');
   }
 
   create() {
