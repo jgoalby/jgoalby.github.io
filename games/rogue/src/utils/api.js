c845f10bd7b2a712385d741df14602695c52044c
John Goalby <jgoalby@gmail.com>
2024-01-15 19:03:12 -0800

    Update api.js

--- games/rogue/src/utils/api.js
+++ games/rogue/src/utils/api.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 
-import 'regenerator-runtime/runtime';
+//import 'regenerator-runtime/runtime';
 
 export default class Api {
   static basePostUrl() {
@@ -12,36 +12,8 @@ export default class Api {
   }
 
   static async post(name, score) {
-    const data = {
-      user: name,
-      score,
-    };
-    try {
-      const response = await fetch(Api.basePostUrl(), {
-        mode: 'cors',
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          Accept: 'application/json',
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      });
-      return response;
-    } catch (error) {
-      return error.message;
-    }
   }
 
   static async get() {
-    try {
-      const response = await fetch(Api.baseGetUrl(), {
-        method: 'GET',
-        headers: { 'Content-type': 'application/json;charset=UTF-8' },
-      });
-      const filter = await response.json();
-      return filter;
-    } catch (error) {
-      return error.message;
-    }
   }
 }
