da894af6c1f477196ec9fcfd303bb97ea0b3bf45
John Goalby <jgoalby@gmail.com>
2024-01-19 06:10:32 +0000

    Dunno

--- games/template/service-worker.js
+++ games/template/service-worker.js
@@ -1,19 +1,32 @@
 // @ts-nocheck
 
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static').then(function(cache) {
+      cache.addAll(['/', '/index.html', '/app.js', '/manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+});
+
 self.addEventListener('fetch', (event) => {
   const version = 'version1';
   
   event.respondWith(
     caches.open(version).then((cache) => {
-      alert("2");
+      console.log("2");
       return cache.match(event.request).then((response) => {
-        alert("3");
+        console.log("3");
         let fetchPromise = fetch(event.request).then((networkResponse) => {
-          alert("4");
+          console.log("4");
           cache.put(event.request, networkResponse.clone());
           return networkResponse;
         });
-        alert("5");
+        console.log("5");
         event.waitUntil(fetchPromise);
         return response;
       })
