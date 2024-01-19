94fcb5d61fa693e0269209f2ebb4041cd40cf96b
John Goalby <jgoalby@gmail.com>
2024-01-19 06:03:04 +0000

    Alerting

--- games/template/service-worker.js
+++ games/template/service-worker.js
@@ -5,15 +5,15 @@ self.addEventListener('fetch', (event) => {
   
   event.respondWith(
     caches.open(version).then((cache) => {
-      console.log("2");
+      alert("2");
       return cache.match(event.request).then((response) => {
-        console.log("3");
+        alert("3");
         let fetchPromise = fetch(event.request).then((networkResponse) => {
-          console.log("4");
+          alert("4");
           cache.put(event.request, networkResponse.clone());
           return networkResponse;
         });
-        console.log("5");
+        alert("5");
         event.waitUntil(fetchPromise);
         return response;
       })
