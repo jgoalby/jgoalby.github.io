78f60a24681f2928b9f5244c79eb204fe366c9f6
John Goalby <jgoalby@gmail.com>
2024-01-31 19:07:49 +0000

    Checking

--- games/template/src/components/CheckBoxButton.js
+++ games/template/src/components/CheckBoxButton.js
@@ -1,12 +1,12 @@
 export default class CheckBoxButton extends Phaser.GameObjects.Container {
-  constructor(scene, x, y, normal, checked, text, initialValue, callback) {
+  constructor(scene, x, y, checked, unchecked, text, initialValue, callback) {
     super(scene);
     this.scene = scene;
     this.x = x;
     this.y = y;
     this.currentValue = initialValue;
 
-    this.button = this.scene.add.image(0, 0, this.currentValue ? checked : normal);
+    this.button = this.scene.add.image(0, 0, this.currentValue ? checked : unchecked);
     this.text = this.scene.add.text(this.button.x + 50, this.button.y, text, {
       fontSize: 24,
     });
@@ -20,7 +20,7 @@ export default class CheckBoxButton extends Phaser.GameObjects.Container {
 
     this.button.on('pointerdown', () => {
       this.currentValue = !this.currentValue;
-      this.button.setTexture(this.currentValue ? checked : normal);
+      this.button.setTexture(this.currentValue ? checked : unchecked);
       callback(this.currentValue);
     });
 
