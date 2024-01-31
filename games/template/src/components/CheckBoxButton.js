e6801af26c07149dc5357bdb23078e8edae20b93
John Goalby <jgoalby@gmail.com>
2024-01-31 19:28:32 +0000

    better pos?

--- games/template/src/components/CheckBoxButton.js
+++ games/template/src/components/CheckBoxButton.js
@@ -6,13 +6,13 @@ export default class CheckBoxButton extends Phaser.GameObjects.Container {
     this.y = y;
 
     this.button = this.scene.add.image(0, 0, getState() ? checked : unchecked);
-    this.button.setOrigin(0.5, 0.5);
+    this.button.setOrigin(0, 0);
     this.button.setInteractive();
 
     this.text = this.scene.add.text(0, 0, text, {
       fontSize: 24,
     });
-    this.text.setOrigin(0.5, 0.5);
+    this.text.setOrigin(0, 0.5);
     this.text.setPosition(this.button.x + this.button.width + 10, this.button.y + (this.button.height / 2));
 
     this.height = (this.text.y + this.text.height) - this.button.y;
