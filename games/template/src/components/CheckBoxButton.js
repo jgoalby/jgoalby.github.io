93ab116b153b8ea185d392f1dbdca05d5bc9f867
John Goalby <jgoalby@gmail.com>
2024-01-31 21:32:38 +0000

    Zoned in

--- games/template/src/components/CheckBoxButton.js
+++ games/template/src/components/CheckBoxButton.js
@@ -12,23 +12,27 @@ export default class CheckBoxButton extends Phaser.GameObjects.Container {
 
     this.button = this.scene.add.image(0, 0, getState() ? checked : unchecked);
     this.button.setOrigin(0, 0);
-    this.button.setInteractive();
+    //this.button.setInteractive();
 
     this.text = this.scene.add.text(0, 0, label, {
       fontSize: 24,
     });
     this.text.setOrigin(0, 0.5);
     this.text.setPosition(this.button.x + this.button.width + 10, this.button.y + (this.button.height / 2));
-    this.text.setInteractive();
+    //this.text.setInteractive();
 
     this.height = (this.text.y + this.text.height) - this.button.y;
     this.width = Math.max(this.button.width, this.text.width);
 
+    this.hitZone = this.scene.add.zone(this.x, this.y, this.width, this.height);
+    this.hitZone.setInteractive();
+
     this.add(this.button);
     this.add(this.text);
 
-    this.button.on('pointerdown', () => { this.checkboxClicked(); });
-    this.text.on('pointerdown',   () => { this.checkboxClicked(); });
+    //this.button.on('pointerdown', () => { this.checkboxClicked(); });
+    //this.text.on('pointerdown',   () => { this.checkboxClicked(); });
+    this.hitZone.on('pointerdown', () => { this.checkboxClicked(); });
 
     this.scene.add.existing(this);
   }
