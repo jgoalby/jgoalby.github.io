510fbbf630bb5cb31519c8bebe5d9f313227c304
John Goalby <jgoalby@gmail.com>
2024-02-19 05:18:46 +0000

    One by One

--- games/template/src/plugins/plugins.js
+++ games/template/src/plugins/plugins.js
@@ -1,5 +1,5 @@
-/*import EventPlugin from '../plugins/EventPlugin.js';
-import SettingsPlugin from '../plugins/SettingsPlugin.js';
+import EventPlugin from '../plugins/EventPlugin.js';
+/*import SettingsPlugin from '../plugins/SettingsPlugin.js';
 import ConsolePlugin from '../plugins/ConsolePlugin.js';
 import IntrospectPlugin from '../plugins/IntrospectPlugin.js';
 import AudioPlugin from '../plugins/AudioPlugin.js';
@@ -7,8 +7,8 @@ import FirebasePlugin from '../plugins/FirebasePlugin.js';
 */
 // List of global plugins, that can be used to initialize the game engine. Order is important.
 const global_plugins = [
-/*  EventPlugin.options,
-  SettingsPlugin.options,
+  EventPlugin.options,
+/*  SettingsPlugin.options,
   ConsolePlugin.options,
   IntrospectPlugin.options,
   AudioPlugin.options,
