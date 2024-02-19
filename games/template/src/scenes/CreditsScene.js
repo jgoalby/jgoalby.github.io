07731df00de479835289867a47156e7382dd1496
John Goalby <jgoalby@gmail.com>
2024-02-19 04:28:22 +0000

    Added plugins text

--- games/template/src/scenes/CreditsScene.js
+++ games/template/src/scenes/CreditsScene.js
@@ -1,21 +1,23 @@
 import Constants from '../constants.js';
 import Button from '../components/ButtonCallback.js';
 import Scenes from './Scenes.js';
+import { getPluginListAsString } from '../plugins/plugins.js'
 
 export default class CreditsScene extends Phaser.Scene {
   constructor() {
     super('Credits');
-    this.heading = null;
-    this.creditsText = null;
-    this.button = null;
+    this.heading = undefined;
+    this.creditsText = undefined;
+    this.pluginsText = undefined;
+    this.button = undefined;
   }
 
   create() {
+    // TODO: Put the controls into a container so we can tween the whole thing.
+
     this.heading = this.add.text(0, 0, 'Credits', Constants.STYLES.HEADING_TEXT);
     this.heading.setOrigin(0.5, 0);
     this.heading.setY(50);
-
-    // TODO: Add plugins and libraries to the credits.
 
     this.creditsText = this.add.text(0, 0, Constants.getCredits(), {
       fontSize: '26px',
@@ -24,22 +26,16 @@ export default class CreditsScene extends Phaser.Scene {
     this.creditsText.setOrigin(0.5, 0);
     this.creditsText.setY(this.heading.y + this.heading.height + 50);
 
-    this.tweens.add({
-      targets: this.heading,
-      y: -500,
-      ease: 'Power1',
-      duration: 5000,
-      delay: 1000,
+    this.pluginsText = this.add.text(0, 0, getPluginListAsString(), {
+      fontSize: '26px',
+      color: '#fff',
     });
+    this.pluginsText.setOrigin(0.5, 0);
+    this.pluginsText.setY(this.creditsText.y + this.creditsText.height + 50);
 
-    this.tweens.add({
-      targets: this.creditsText,
-      y: -500,
-      ease: 'Power1',
-      duration: 10000,
-      delay: 1000,
-      onComplete: () => { this.gotoMainMenu() },
-    });
+    this.tweens.add({targets: this.heading, y: -500, ease: 'Power1', duration: 5000, delay: 1000});
+    this.tweens.add({targets: this.creditsText, y: -500, ease: 'Power1', duration: 10000, delay: 1000});
+    this.tweens.add({targets: this.pluginsText, y: -500 - this.creditsText.height, ease: 'Power1', duration: 10000, delay: 1000, onComplete: () => { this.gotoMainMenu() }});
 
     this.button = new Button(this, 0, 0, 'normalButton', 'hoverButton', 'Menu', () => { this.gotoMainMenu() });
 
@@ -55,6 +51,7 @@ export default class CreditsScene extends Phaser.Scene {
   resize() {
     this.heading.setX(this.cameras.main.width / 2);
     this.creditsText.setX(this.cameras.main.width / 2);
+    this.pluginsText.setX(this.cameras.main.width / 2);
     this.button.setPosition(this.cameras.main.width / 2, this.cameras.main.height - ((this.button.height / 2) + 10));
   }
 }
