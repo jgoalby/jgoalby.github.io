3b6a66f88fcfc7f05b1f0a7cb8179eb0b0b2758b
John Goalby <jgoalby@gmail.com>
2024-02-20 00:49:01 +0000

    Rework

--- games/template/service-worker.js
+++ games/template/service-worker.js
@@ -3,7 +3,6 @@
 const cacheName = "cache-v1";
 
 self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       cache.addAll(['./index.html',
@@ -16,7 +15,6 @@ self.addEventListener('install', function(event) {
 
 /* Cache space is limited, so clean up old caches. */
 self.addEventListener("activate", (e) => {
-  console.log('Activating Service Worker...', e);
   e.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
@@ -32,45 +30,36 @@ self.addEventListener("activate", (e) => {
 });
 
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener('fetch', function(event) {
   // Prevent requests such as chrome plugins.
-  if (!e.request.url.startsWith('http')) {
+  if (!event.request.url.startsWith('http')) {
     return;
   }
 
-  console.log("Fetching: " + e.request.url);
+  // Only deal with GET requests.
+  if (event.request.method != 'GET') {
+    return;
+  }
 
-  e.respondWith(
-    (async () => {
-      console.log("Actual fetch: " + e.request.url);
-      let fetchPromise = fetch(e.request).then(function(res) {
-        console.log("Putting in cache: " + e.request.url)
-        return caches.open(cacheName).then(function(cache) {
-          console.log("Putting in cache real: " + e.request.url)
-          cache.put(e.request.url, res.clone());
-          return res;
-        });
-      }).catch(function(error) {
-        // Do nothing as likely not connected to the internet.
-        console.log("Error fetching: " + e.request.url + " " + error);
-      });
+  event.respondWith((async () => {
+    const response = await fetch(event.request);
+  
+    if (!response || response.status !== 200 || response.type !== 'basic') {
+      return response;
+    }
+  
+    const cache = await caches.open(cacheName);
+    await cache.put(event.request, response.clone());
+  
+    const cachedResponse = await caches.match(event.request);
 
-      console.log("Waiting for fetch: " + e.request.url);
-      e.waitUntil(fetchPromise);
-      console.log("Done with fetch: " + e.request.url);
-
-      const r = await caches.match(e.request);
-
-      if (r) {
-        console.log("Getting from cache: " + e.request.url);
-        return r;
-      } else {
-        console.log("Not in cache: " + e.request.url);
-      }
-    })(),
-  );
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+      return response;
+    }
+  })());
 });
-
 
 /*self.addEventListener('fetch', function(event) {
   // Prevent requests such as chrome plugins.
