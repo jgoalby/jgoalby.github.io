37ea8fe2264e3697c59be8a4432c68bcc5c8b6a5
John Goalby <jgoalby@gmail.com>
2024-03-02 05:43:38 +0000

    Various

--- games/template/src/components/Notification.js
+++ games/template/src/components/Notification.js
@@ -13,10 +13,10 @@ export default class Notification extends Phaser.GameObjects.Container {
     this.y = 0;
 
     const { height, width } = this.scene.scale;
-    this.panel = this.scene.add.nineslice(width * 2, height - (100 + 20), "panel", 0, 400, 100, 32, 32, 32, 32);
+    this.panel = this.scene.add.nineslice(width * 2, height - 20, "panel", 0, 400, 100, 32, 32, 32, 32);
     this.panel.setOrigin(0.5, 1);
 
-    this.scene.tweens.add({targets: this.panel, x: width / 2, ease: 'quart.out', duration: 1000, hold: 1500, yoyo: true, completeDelay: 1000, oncomplete: () => { this.destroy() }});
+    this.scene.tweens.add({targets: this.panel, x: width / 2, ease: 'quart.out', duration: 1000, hold: 1500, yoyo: true, completeDelay: 5000, oncomplete: () => { console.log("WAAAAAA!!!!!"); this.destroy() }});
     
     this.scene.add.existing(this);
   }
