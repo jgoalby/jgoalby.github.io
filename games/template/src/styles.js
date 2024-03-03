615d110e6cacaf53f8643cb5000914be96806120
John Goalby <jgoalby@gmail.com>
2024-03-03 22:35:51 +0000

    Anyway

--- games/template/src/styles.js
+++ games/template/src/styles.js
@@ -1,7 +1,23 @@
+const BACKGROUND_COLOR = '#000000';
+
+const Styles = `{
+  "BACKGROUND_COLOR": "${BACKGROUND_COLOR}",
+  "MAIN_FONT": "Arial",
+  "BODY_TEXT_FONT": "Arial",
+  "BODY_TEXT_COLOR": "#ffffff",
+  "BODY_TEXT_SIZE": 24,
+  "BODY_TEXT": {
+    "fontfamily": "Arial",
+    "fontSize": 24,
+    "color": "#ffffff"
+  }
+}`
+
 function doit() {
-  //const template = Handlebars.compile("Name: {{name}}");
-  //console.log(template({ name: "Nils" }));
   console.log('styles.js doit()');
+
+  const styles = JSON.parse(Styles);
+  console.log(styles);
 }
 
 export {
