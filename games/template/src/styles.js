c50b3a2eefb49114ce70666e60169ac0dbfbf0e7
John Goalby <jgoalby@gmail.com>
2024-03-03 23:39:36 +0000

    Weird

--- games/template/src/styles.js
+++ games/template/src/styles.js
@@ -1,5 +1,46 @@
 import * as Sqrl from 'squirrelly'
 
+/*function deferWithDefaults([first, ...rest], ...defaults) {
+  return (...values) => rest.reduce((acc, curr, i) => {
+    return acc + (i < values.length ? values[i] : defaults[i]) + curr;
+  }, first);
+}*/
+
+/*
+function deferWithDefaults(strs, ...defaults) {
+  var [first, ...rest] = strs;
+  return (...values) => rest.reduce((acc, curr, i) => {
+    return acc + (i < values.length ? values[i] : defaults[i]) + curr;
+  }, first);
+}
+
+let t = deferWithDefaults`My template is: ${'extendable'} and ${'versatile'}`;
+let a = t('awesome');
+
+console.log(a);
+
+*/
+
+// 'My template is: awesome and versatile' 
+
+
+function defer(strs, ...defaults) {
+  var [first, ...rest] = strs;
+  return (...vals) => rest.reduce((acc, str, i) => acc + vals[i] + str, first);
+}
+
+let t = defer`My template is: ${null} and ${null}`;
+let a = t('simple', 'reusable');
+// 'My template is: simple and reusable'
+let b = t('obvious', 'late to the party');
+// 'My template is: obvious and late to the party'
+let c = t(null);
+// 'My template is: null and undefined'
+let d = defer`Choose: ${'ignore'} / ${undefined}`(true, false);
+// 'Choose: true / false'
+
+console.log(a + "\n" + b + "\n" + c + "\n" + d + "\n");
+
 const BaseStyles = {
   BACKGROUND_COLOR: '#000000',
 }
