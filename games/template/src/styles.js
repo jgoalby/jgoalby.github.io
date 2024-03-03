4ccd4941e0bced154d8ced9abfac65afc07c9ac4
John Goalby <jgoalby@gmail.com>
2024-03-03 22:28:01 +0000

    Remove handlebars

--- games/template/src/styles.js
+++ games/template/src/styles.js
@@ -1,8 +1,6 @@
-import { Handlebars } from './lib/handlebars.min-v4.7.8';
-
 function doit() {
-  const template = Handlebars.compile("Name: {{name}}");
-  console.log(template({ name: "Nils" }));
+  //const template = Handlebars.compile("Name: {{name}}");
+  //console.log(template({ name: "Nils" }));
   console.log('styles.js doit()');
 }
 
