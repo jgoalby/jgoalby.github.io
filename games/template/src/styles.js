3ab8a0a97461d7cb0478f701b800fcb4380f2678
John Goalby <jgoalby@gmail.com>
2024-03-03 22:04:05 +0000

    Ew

--- games/template/src/styles.js
+++ games/template/src/styles.js
@@ -1,4 +1,4 @@
-import { Handlebars } from './lib/handlebars.min-v4.7.8';
+//import { Handlebars } from './lib/handlebars.min-v4.7.8';
 
 function doit() {
   //const template = Handlebars.compile("Name: {{name}}");
