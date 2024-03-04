485b07175667bbd304df23e29724a31aca4a058c
John Goalby <jgoalby@gmail.com>
2024-03-04 01:37:29 +0000

    Cleaned up a little

--- games/template/src/styles.js
+++ games/template/src/styles.js
@@ -1,56 +1,3 @@
-/*function deferWithDefaults([first, ...rest], ...defaults) {
-  return (...values) => rest.reduce((acc, curr, i) => {
-    return acc + (i < values.length ? values[i] : defaults[i]) + curr;
-  }, first);
-}*/
-
-/*
-function deferWithDefaults(strs, ...defaults) {
-  var [first, ...rest] = strs;
-  return (...values) => rest.reduce((acc, curr, i) => {
-    return acc + (i < values.length ? values[i] : defaults[i]) + curr;
-  }, first);
-}
-
-let t = deferWithDefaults`My template is: ${'extendable'} and ${'versatile'}`;
-let a = t('awesome');
-
-console.log(a);
-
-*/
-
-// 'My template is: awesome and versatile' 
-
-
-/*function defer(strs, ...defaults) {
-  var [first, ...rest] = strs;
-  return (...vals) => rest.reduce((acc, str, i) => acc + vals[i] + str, first);
-}*/
-
-/*function defer(strs, ...defaults) {
-  console.log('-- defer --');
-  console.log(strs);
-  var [first, ...rest] = strs;
-  console.log(first);
-  console.log(rest);
-  return (...values) => rest.reduce((acc, curr, i) => {
-    return acc + (i < values.length ? values[i] : defaults[i]) + curr;
-  }, first);
-}
-
-let t = defer`My template is: ${'extendable'} and ${'versatile'}`;
-let a = t('simple', 'reusable');
-// 'My template is: simple and reusable'
-let b = t('obvious', 'late to the party');
-// 'My template is: obvious and late to the party'
-let c = t(null);
-// 'My template is: null and undefined'
-let d = defer`Choose: ${'ignore'} / ${undefined}`(true, false);
-// 'Choose: true / false'
-
-console.log(a + "\n" + b + "\n" + c + "\n" + d + "\n");
-*/
-
 const isUndefined = o => typeof o === 'undefined'
 
 const nvl = (o, valueIfUndefined) => isUndefined(o) ? valueIfUndefined : o
@@ -127,27 +74,6 @@ const resolveTemplate = (source, variables) => {
 }
 
 function doit() {
-  /*const styles = JSON.parse(Styles);
-  console.log(styles);
-
-  // setup variables for resolution...
-  var variables = {}
-  variables['top level'] = 'Foo'
-  variables['deep object'] = {text:'Bar'}
-  
-  // ==> Foo Bar <==
-  console.log(resolveTemplate('==> ${top level} ${deep object.text} <==', variables))
-
-  // ==> Dog Dog <==
-  console.log(resolveTemplate('==> ${aGlobalVariable} ${aGlobalVariable} <==', { aGlobalVariable: 'Dog' }))
-
-  // ==> ${not an object.text} <==
-  console.log(resolveTemplate('==> ${not an object.text} <==', variables))
-
-  // ==> 5Foobar <==
-  console.log(resolveTemplate2('==> ${1 + 4 + this.someVal} <==', {someVal: 'Foobar'}))
-*/
-
   // Remove the comment lines from the StylesJSON
   const cleanedStylesJSON = StylesJSON.replace(/\/\/.*/g, '');
 
