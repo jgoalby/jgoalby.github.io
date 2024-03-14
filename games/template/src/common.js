66f18a103f8abb36926a2cb0910938e3121c2555
John <jgoalby@gmail.com>
2024-03-14 12:19:43 -0700

    Removed Jasmine

--- games/template/src/common.js
+++ games/template/src/common.js
@@ -313,19 +313,6 @@ function testModule() {
   testKeyEventToString();
 }
 
-// Jasmine testing
-
-describe('Player', function() {
-
-  beforeEach(function() {
-  });
-
-  it('should be working', function() {
-    //expect(player).toBePlaying(song);
-  });
-});
-
-
 export {
   assert,
   assertEqual,
